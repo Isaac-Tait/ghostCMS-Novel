c9553bfd864cedff1801b61e233d5203c5444624
Isaac Tait <isaac@mountaintopcoding.com>
2022-09-05 11:51:50 -0400

    feat: add posts to home page 🌹

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,41 @@
-//import Link from 'next/link';
+import Link from 'next/link';
+import { getPosts } from '../lib/posts';
 
 import Footer from '../components/Footer';
-import Header from '../components/Header';
 
-const Index = () => (
+export const getStaticProps = async () => {
+  const posts = await getPosts();
+
+  if (!posts) {
+    return {
+      notFound: true,
+    };
+  }
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default function Home({ posts }) {
   <div className="heropattern-leaf-neutral-100 h-screen">
-    <div>
-      <Header />
-    </div>
-    <div className="max-w-6xl mx-auto bg-slate-200 rounded-xl">
-      <p>Posts</p>
-    </div>
+    <main>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => {
+          return (
+            <li key={post.slug}>
+              <Link href="/posts/[slug]" as={`/posts/${post.slug}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
     <div className="fixed bottom-0 w-full">
       <Footer />
     </div>
-  </div>
-);
-
-export default Index;
+  </div>;
+}
